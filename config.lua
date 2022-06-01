20ad98469787f80dde0425e8de917ecd3404439e
0x736d <subash.mahapatra@proton.me>
2022-06-01 14:29:14 +0530

    fix: update to nvim-tree config LunarVim/LunarVim#2681

--- config.lua
+++ config.lua
@@ -28,7 +28,7 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.active = true
 -- debugger adapter
 lvim.builtin.dap.active = true
 
